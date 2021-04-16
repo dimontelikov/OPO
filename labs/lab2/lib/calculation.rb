f14e220591a2ec56ea244cf600dc97d780dd0720
DmitryTelyakov <dimontelikotelikov@gmail.com>
2021-04-16 17:13:53 +0700

    2

--- labs/lab2/lib/calculation.rb
+++ labs/lab2/lib/calculation.rb
@@ -15,7 +15,7 @@ class Calculation
     (@data.inject { |sum, elem| sum + elem }.to_f / @data.size).round(2)
   end
 
-  def search_corrected_sample_variance # Дисперсия
+  def search_corrected_sample_variance # dispersion
     mean = search_mean
     sum = @data.map { |elem| (elem - mean)**2 }
     (sum.inject(:+) / (@data.size - 1)).round(2)
